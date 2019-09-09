cb7b039d2706ec3240ad078c4f66fa8e24677650
Leo Singleton <leo@leosingleton.com>
2019-09-09 08:00:17 +0200

    Copying files from FIM to initialize repo

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const webpack = require('webpack');
+const path = require('path');
+
+var config = {
+  entry: './src/index.ts',
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.glsl$/,
+        use: 'webpack-glsl-minify'
+      }
+    ]
+  },
+  resolve: {
+    extensions: [ '.glsl', '.ts' ]
+  },
+  externals: [
+    '@leosingleton/commonlibs'
+  ],
+  output: {
+    path: path.resolve(__dirname, 'build/dist'),
+    filename: 'index.js',
+    library: 'library',
+    libraryTarget: 'commonjs2',
+    umdNamedDefine: true,
+    globalObject: '(typeof self !== "undefined" ? self : this)'
+  }
+};
+
+module.exports = config;
