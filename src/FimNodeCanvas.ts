df174184b31a5f480d12dc83552a403acf492416
Leo Singleton <leo@leosingleton.com>
2019-09-12 21:32:08 -0400

    Making FimNodeCanvas a derived class, not just functions

--- src/FimNodeCanvas.ts
+++ src/FimNodeCanvas.ts
@@ -4,15 +4,47 @@
 
 import { NodeOffscreenCanvasFactory } from './NodeOffscreenCanvas';
 import { using, IDisposable } from '@leosingleton/commonlibs';
-import { FimCanvas } from '@leosingleton/fim';
+import { FimCanvas, FimColor } from '@leosingleton/fim';
 import { CanvasRenderingContext2D, Image } from 'canvas';
 
-export namespace FimNodeCanvas {
+/**
+ * Enhanced version of FimCanvas to support Node.js
+ */
+export class FimNodeCanvas extends FimCanvas {
+  /**
+   * Creates an invisible canvas
+   * @param width Canvas width, in pixels
+   * @param height Canvas height, in pixels
+   * @param initialColor If specified, the canvas is initalized to this color.
+   */
+  public constructor(width: number, height: number, initialColor?: string | FimColor) {
+    super(width, height, initialColor, NodeOffscreenCanvasFactory);
+  }
+
+  /** Creates a new FimCanvas which is a duplicate of this one */
+  public duplicateCanvas(): FimNodeCanvas {
+    let dupe = new FimNodeCanvas(this.imageDimensions.w, this.imageDimensions.h);
+    dupe.copyFromCanvas(this, this.imageDimensions, this.imageDimensions);
+    return dupe;
+  }
+  
+  /**
+   * Constructs a drawing context
+   * @param imageSmoothingEnabled Enables image smoothing
+   * @param operation CanvasRenderingContext2D.globalCompositeOperation value, e.g. 'copy' or 'source-over'
+   * @param alpha CanvasRenderingContext2D.alpha value, where 0 = transparent and 1 = opaque
+   */
+  public createDrawingContext(imageSmoothingEnabled = false, operation = 'copy', alpha = 1):
+      CanvasRenderingContext2D & IDisposable {
+    return super.createDrawingContext(imageSmoothingEnabled, operation, alpha) as
+      (CanvasRenderingContext2D & IDisposable);
+  }
+
   /**
    * Creates a FimCanvas from a JPEG file
    * @param jpegFile JPEG file, loaded into a byte array
    */
-  export function createFromJpeg(jpegFile: Uint8Array): Promise<FimCanvas> {
+  public static createFromJpeg(jpegFile: Uint8Array): Promise<FimNodeCanvas> {
     // Create a Blob holding the binary data and load it onto an HTMLImageElement
     let buffer = Buffer.from(jpegFile);
     return FimNodeCanvas.createFromImageBuffer(buffer);
@@ -22,14 +54,14 @@ export namespace FimNodeCanvas {
    * Creates a FimCanvas from a Blob containing an image
    * @param buffer Buffer containing an image encoded in JPEG or PNG format
    */
-  export function createFromImageBuffer(buffer: Buffer): Promise<FimCanvas> {
+  public static createFromImageBuffer(buffer: Buffer): Promise<FimNodeCanvas> {
     return new Promise((resolve, reject) => {
       let img = new Image();
 
       // On success, copy the image to a FimCanvas and return it via the Promise
       img.onload = () => {
-        let result = new FimCanvas(img.width, img.height, undefined, NodeOffscreenCanvasFactory);
-        using(result.createDrawingContext() as any as CanvasRenderingContext2D & IDisposable, ctx => {
+        let result = new FimNodeCanvas(img.width, img.height);
+        using(result.createDrawingContext(), ctx => {
           ctx.drawImage(img, 0, 0);
         });
 
