0e936ad4cfed9f91d843122d149a0ae6fa59741d
Leo Singleton <leo@leosingleton.com>
2019-09-13 06:18:32 -0400

    Adding more unit tests

--- src/__tests__/NodeOffscreenCanvas.test.ts
+++ src/__tests__/NodeOffscreenCanvas.test.ts
@@ -16,4 +16,50 @@ describe('NodeOffscreenCanvas', () => {
     oc.dispose();
   });
 
+  it('Gets a Canvas2D drawing context', () => {
+    let oc = new NodeOffscreenCanvas(100, 200);
+    let ctx = oc.getContext('2d') as OffscreenCanvasRenderingContext2D;
+    expect(ctx).toBeDefined();
+
+    ctx.fillStyle = '#00f';
+    ctx.rect(20, 20, 80, 180);
+    ctx.fill();
+    oc.dispose();    
+  });
+
+  it('Gets a WebGL rendering context', () => {
+    let oc = new NodeOffscreenCanvas(100, 200);
+    let ctx = oc.getContext('webgl') as WebGLRenderingContext;
+    expect(ctx).toBeDefined();
+  });
+
+  it('Exports to JPEG', async () => {
+    let oc = new NodeOffscreenCanvas(100, 200);
+    let ctx = oc.getContext('webgl') as WebGLRenderingContext;
+    expect(ctx).toBeDefined();
+
+    let buffer = await oc.convertToBuffer({ type: 'image/jpeg', quality: 0.9 });
+    let jpeg = new Uint8Array(buffer);
+
+    // JPEG magic number is FF D8 FF
+    expect(jpeg[0]).toBe(0xff);
+    expect(jpeg[1]).toBe(0xd8);
+    expect(jpeg[2]).toBe(0xff);
+  });
+
+  it('Exports to PNG', async () => {
+    let oc = new NodeOffscreenCanvas(100, 200);
+    let ctx = oc.getContext('webgl') as WebGLRenderingContext;
+    expect(ctx).toBeDefined();
+
+    let buffer = await oc.convertToBuffer({ type: 'image/png' });
+    let png = new Uint8Array(buffer);
+
+    // PNG magic number is 89 50 4E 47 (ASCII for .PNG)
+    expect(png[0]).toBe(0x89);
+    expect(png[1]).toBe(0x50);
+    expect(png[2]).toBe(0x4e);
+    expect(png[3]).toBe(0x47);
+  });
+
 });
