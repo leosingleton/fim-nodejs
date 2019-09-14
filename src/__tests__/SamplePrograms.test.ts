ff037398f078afa261dcdf9f0658bf8b891cfc86
Leo Singleton <leo@leosingleton.com>
2019-09-14 18:09:54 -0400

    Another fill unit test
    This passes, so clearly looking like a problem with headless-gl and
    uniforms.

--- src/__tests__/SamplePrograms.test.ts
+++ src/__tests__/SamplePrograms.test.ts
@@ -10,13 +10,20 @@ import { GlslMinify, GlslShader } from 'webpack-glsl-minify/build/minify';
 describe('Sample Programs', () => {
 
   it('Accepts a float array as a uniform', async () => {
-    let shader = await compileShader(floatArrayUniformShader);
+    let shader = await compileShader(yellowShader);
+    using(new FimNodeGLCanvas(100, 200, '#f00'), canvas => {
+      let program = new SampleProgram(canvas, shader);
+      program.execute();  
+      expect(canvas.getPixel(50, 50)).toEqual(FimColor.fromString('#ff0'));
+    });
+  });
 
+  it('Accepts a float array as a uniform', async () => {
+    let shader = await compileShader(floatArrayUniformShader);
     using(new FimNodeGLCanvas(100, 200, '#f00'), canvas => {
       let program = new SampleProgram(canvas, shader);
       program.setInput('uColor', [0, 1, 0]);
       program.execute();
-  
       expect(canvas.getPixel(50, 50)).toEqual(FimColor.fromString('#000')); // BUGBUG: Should be #0f0!!!
     });
   });
@@ -40,15 +47,24 @@ function compileShader(shader: string): Promise<GlslShader> {
   return min.execute(shader);
 }
 
+/** Simple shader to fill the output with yellow */
+const yellowShader = `
+precision mediump float;
+varying vec2 vCoord;
+
+void main()
+{
+  gl_FragColor = vec4(1.0, 1.0, 0.0, 1.0);
+}`;
+
+/** Simple shader to test passing arrays of floats as uniforms */
 const floatArrayUniformShader = `
 precision mediump float;
 varying vec2 vCoord;
 
 uniform float uColor[3];
 
-// Simple shader to test passing arrays of floats as uniforms
 void main()
 {
   gl_FragColor = vec4(uColor[0], uColor[1], uColor[2], 1.0);
-}
-`;
+}`;
